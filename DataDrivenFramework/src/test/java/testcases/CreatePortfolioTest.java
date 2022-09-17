5100acd15560acb162d7484f74f122ef646370a3
rahul rohit <er.rahu@gmail.com>
2022-09-17 17:20:01 +0530

    Rahul

---
+++ DataDrivenFramework/src/test/java/testcases/CreatePortfolioTest.java
@@ -0,0 +1,38 @@
+package testcases;
+
+import org.testng.annotations.Test;
+
+import keywords.ApplicationKeywords;
+import keywords.GenericKeywords;
+import keywords.ValidationKeywords;
+
+public class CreatePortfolioTest {
+
+	
+	@Test
+	public void createPortFolioTest() {
+	   // no webdriver code
+       // login
+	   // create
+	   // verify
+		
+	   ApplicationKeywords app = new ApplicationKeywords();// init prop
+	   
+	   app.openBrowser("Chrome");
+	   app.navigate("url");
+	   app.type("username_css", "ashishthakur1983");
+	   app.type("password_xpath", "pass@1234");
+	   app.validateElementPresent("login_submit_id");
+	   app.click("login_submit_id");
+	   app.validateLogin();
+	   //app.selectDateFromCalendar();	
+	}
+	
+	@Test
+	public void createReservation() {
+		
+	}
+	
+	
+	
+}
